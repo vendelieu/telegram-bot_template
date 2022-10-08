b5691630bdd3d3e5228d7772c0eb2b802a5d624a
vendelieu <vennywise>
2022-10-08 21:49:04 +0300

    add basic example of handling

---
+++ src/main/kotlin/eu/vendeli/samples/controller/StartController.kt
@@ -0,0 +1,13 @@
+package eu.vendeli.samples.controller
+
+import eu.vendeli.tgbot.TelegramBot
+import eu.vendeli.tgbot.annotations.CommandHandler
+import eu.vendeli.tgbot.api.message
+import eu.vendeli.tgbot.types.internal.ProcessedUpdate
+
+class StartController {
+    @CommandHandler(["/start"])
+    suspend fun start(update: ProcessedUpdate, bot: TelegramBot) {
+        message("Hello").send(update.user, bot)
+    }
+}
