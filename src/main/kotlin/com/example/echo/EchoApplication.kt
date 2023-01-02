cd2bcd0c6fde7c65ff7ef122ea4d274354b52ca7
vendelieu <vendelieu@gmail.com>
2023-01-02 20:40:27 +0100

    update lib ver to 2.5.0

--- src/main/kotlin/com/example/echo/EchoApplication.kt
+++ src/main/kotlin/com/example/echo/EchoApplication.kt
@@ -6,12 +6,9 @@ import eu.vendeli.tgbot.api.message
 suspend fun main() {
     val bot = TelegramBot("BOT_TOKEN")
 
-    bot.update.setListener {
-        message(it.message?.text ?: "").send(it.message?.from?.id ?: 0, bot)
-//        handle(it) { // same in manual handling mode
-//            onMessage {
-//                message { data.message?.text ?: "" }.send(data.message?.from ?: 0, bot)
-//            }
-//        }
+    bot.handleUpdates {
+        onMessage {
+            message(data.text ?: "").send(data.from?.id ?: 0, bot)
+        }
     }
 }
