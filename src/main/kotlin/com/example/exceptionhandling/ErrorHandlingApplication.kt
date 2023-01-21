238a9ab78220eb62c37faf2efb30438cc54e3603
vendelieu <vendelieu@gmail.com>
2023-01-21 19:40:30 +0100

    bump lib ver to 2.5.4

--- src/main/kotlin/com/example/exceptionhandling/ErrorHandlingApplication.kt
+++ src/main/kotlin/com/example/exceptionhandling/ErrorHandlingApplication.kt
@@ -2,7 +2,7 @@ package com.example.exceptionhandling
 
 import eu.vendeli.tgbot.TelegramBot
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.channels.consumeEach
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
@@ -10,8 +10,9 @@ fun main() = runBlocking {
     val bot = TelegramBot("BOT_TOKEN", "com.example.exceptionhandling.controller")
 
     launch(Dispatchers.Unconfined) {
-        bot.update.caughtExceptions.consumeEach {
-            ExceptionHandler.handleException(it.second, it.first)
+        for (e in bot.update.caughtExceptions) {
+            ExceptionHandler.handleException(e.second, e.first)
+            delay(100)
         }
     }
     bot.handleUpdates()
