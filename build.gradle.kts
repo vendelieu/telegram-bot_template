aee577599a289f2a672c9708cdc88e360c1aeeab
vendelieu <vendelieu@gmail.com>
2023-03-27 23:14:10 +0200

    move dependency versions management to libs.versions.toml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
+val jvmTargetVersion = JavaVersion.VERSION_11
+
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     application
-    kotlin("jvm") version "1.8.0"
+    alias(libs.plugins.kotlin.jvm)
 }
 
 group = "com.example.echo"
@@ -14,5 +17,14 @@ repositories {
 }
 
 dependencies {
-    implementation("eu.vendeli:telegram-bot:2.6.0")
+    implementation(libs.tg.bot)
+}
+
+tasks {
+    compileJava {
+        targetCompatibility = jvmTargetVersion.majorVersion
+    }
+    compileKotlin {
+        kotlinOptions.jvmTarget = jvmTargetVersion.majorVersion
+    }
 }
