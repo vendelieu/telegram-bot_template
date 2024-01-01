bff776a3ebd51abb401a7bc19349fea10a090c3e
vendelieu <vendelieu@gmail.com>
2024-01-01 17:29:31 +0300

    update lib

--- src/main/kotlin/com/example/blank/TgBotApplication.kt
+++ src/main/kotlin/com/example/blank/TgBotApplication.kt
@@ -1,9 +1,8 @@
 package com.example.blank
 
 import eu.vendeli.tgbot.TelegramBot
-import kotlinx.coroutines.runBlocking
 
-fun main() = runBlocking {
+suspend fun main() {
     val bot = TelegramBot("BOT_TOKEN", "com.example.blank.controller")
     // And that's all write your own bot from blank.
     // Create new controllers in controller folder or expand old one
