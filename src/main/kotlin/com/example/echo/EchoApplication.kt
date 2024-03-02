f5df7f51a951de29aeec854b4ca96346d441c9bb
vendelieu <vendelieu@gmail.com>
2024-03-02 13:08:16 +0300

    update lib

--- src/main/kotlin/com/example/echo/EchoApplication.kt
+++ src/main/kotlin/com/example/echo/EchoApplication.kt
@@ -1,7 +1,7 @@
 package com.example.echo
 
 import eu.vendeli.tgbot.TelegramBot
-import eu.vendeli.tgbot.api.message
+import eu.vendeli.tgbot.api.message.message
 
 suspend fun main() {
     val bot = TelegramBot("BOT_TOKEN")
