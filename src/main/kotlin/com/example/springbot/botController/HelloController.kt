822de747951b4c33a33f494dd0d6d28b6f146ed8
vendelieu <vendelieu@gmail.com>
2024-03-02 13:11:44 +0300

    update lib

--- src/main/kotlin/com/example/springbot/botController/HelloController.kt
+++ src/main/kotlin/com/example/springbot/botController/HelloController.kt
@@ -2,7 +2,7 @@ package com.example.springbot.botController
 
 import eu.vendeli.tgbot.TelegramBot
 import eu.vendeli.tgbot.annotations.CommandHandler
-import eu.vendeli.tgbot.api.message
+import eu.vendeli.tgbot.api.message.message
 import eu.vendeli.tgbot.types.User
 import org.springframework.stereotype.Component
 
