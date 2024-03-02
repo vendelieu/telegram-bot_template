22ae84fc7f22c64d74858218e22f756da84bb40e
vendelieu <vendelieu@gmail.com>
2024-03-02 13:10:19 +0300

    update lib

--- src/main/kotlin/com/example/heroku/controller/HelloController.kt
+++ src/main/kotlin/com/example/heroku/controller/HelloController.kt
@@ -2,7 +2,7 @@ package com.example.heroku.controller
 
 import eu.vendeli.tgbot.TelegramBot
 import eu.vendeli.tgbot.annotations.CommandHandler
-import eu.vendeli.tgbot.api.message
+import eu.vendeli.tgbot.api.message.message
 import eu.vendeli.tgbot.types.User
 import eu.vendeli.tgbot.types.internal.ProcessedUpdate
 
