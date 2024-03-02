8ceb87f5dfcbc7a023891c48699a5932f0786f20
vendelieu <vendelieu@gmail.com>
2024-03-02 13:09:21 +0300

    update lib

--- src/main/kotlin/com/example/poll/controller/PollController.kt
+++ src/main/kotlin/com/example/poll/controller/PollController.kt
@@ -2,7 +2,7 @@ package com.example.poll.controller
 
 import eu.vendeli.tgbot.TelegramBot
 import eu.vendeli.tgbot.annotations.CommandHandler
-import eu.vendeli.tgbot.api.message
+import eu.vendeli.tgbot.api.message.message
 import eu.vendeli.tgbot.api.poll
 import eu.vendeli.tgbot.types.PollType
 import eu.vendeli.tgbot.types.User
