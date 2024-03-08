b837399f906024c9d88b53c001b37155b9cd2ffd
vendelieu <vendelieu@gmail.com>
2024-03-08 03:34:03 +0300

    fix to new classmanager

--- src/main/kotlin/com/example/springbot/implementation/SpringClassManager.kt
+++ src/main/kotlin/com/example/springbot/implementation/SpringClassManager.kt
@@ -3,11 +3,12 @@ package com.example.springbot.implementation
 import eu.vendeli.tgbot.interfaces.ClassManager
 import org.springframework.context.ApplicationContext
 import org.springframework.stereotype.Component
+import kotlin.reflect.KClass
 
 @Component
 class SpringClassManager(
     private val applicationContext: ApplicationContext
 ) : ClassManager {
-    override fun getInstance(clazz: Class<*>, vararg initParams: Any?): Any =
-        applicationContext.getBean(clazz, initParams)
+    override fun getInstance(kClass: KClass<*>, vararg initParams: Any?): Any =
+        applicationContext.getBean(kClass.java, initParams)
 }
