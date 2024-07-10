cd71e4683860fc23fbe58aa1a24114dfc74f4e38
vendelieu <vendelieu@gmail.com>
2024-07-11 01:32:06 +0300

    update version

--- src/main/kotlin/com/example/exceptionhandling/ExceptionHandler.kt
+++ src/main/kotlin/com/example/exceptionhandling/ExceptionHandler.kt
@@ -1,12 +1,12 @@
 package com.example.exceptionhandling
 
-import eu.vendeli.tgbot.types.Update
+import eu.vendeli.tgbot.types.internal.ProcessedUpdate
 import org.slf4j.LoggerFactory
 
 object ExceptionHandler {
     private val logger = LoggerFactory.getLogger(this::class.java)
 
-    fun handleException(update: Update, ex: Throwable) {
+    fun handleException(update: ProcessedUpdate, ex: Throwable) {
         logger.info("Received an exception while processing an update: $update", ex)
     }
 }
