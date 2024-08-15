a4eece986af2e74dc4618be9c59030f0bc136443
vendelieu <vendelieu@gmail.com>
2024-08-15 13:39:33 +0300

    update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ dependencies {
 
 kotlin {
     compilerOptions {
-        jvmTarget = JvmTarget.valueOf(jvmTargetVersion.majorVersion)
+        jvmTarget = JvmTarget.fromTarget(jvmTargetVersion.majorVersion)
     }
     jvmToolchain(jvmTargetVersion.majorVersion.toInt())
 }
