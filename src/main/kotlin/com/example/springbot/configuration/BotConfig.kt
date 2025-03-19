0beaa9ccd4a7b9a853ddea61c01fce4737a1c1da
vendelieu <vendelieu@gmail.com>
2025-03-19 15:18:51 +0300

    upgrade

--- src/main/kotlin/com/example/springbot/configuration/BotConfig.kt
+++ src/main/kotlin/com/example/springbot/configuration/BotConfig.kt
@@ -1,8 +1,8 @@
 package com.example.springbot.configuration
 
 import eu.vendeli.spring.starter.BotConfiguration
-import eu.vendeli.tgbot.types.internal.LogLvl
-import eu.vendeli.tgbot.utils.BotConfigurator
+import eu.vendeli.tgbot.types.component.LogLvl
+import eu.vendeli.tgbot.utils.common.BotConfigurator
 import org.springframework.context.annotation.Configuration
 
 @Configuration
