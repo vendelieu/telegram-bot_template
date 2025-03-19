df537b0dfac8c927ab99e43d9d46baeec2d83cec
vendelieu <vendelieu@gmail.com>
2025-03-19 15:31:12 +0300

    upgrade to 8

--- src/main/kotlin/com/example/echo/EchoApplication.kt
+++ src/main/kotlin/com/example/echo/EchoApplication.kt
@@ -2,7 +2,7 @@ package com.example.echo
 
 import eu.vendeli.tgbot.TelegramBot
 import eu.vendeli.tgbot.api.message.message
-import eu.vendeli.tgbot.utils.onMessage
+import eu.vendeli.tgbot.utils.common.onMessage
 
 suspend fun main() {
     val bot = TelegramBot("BOT_TOKEN")
